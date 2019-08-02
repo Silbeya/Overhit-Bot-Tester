c504ca146823c1e08c48f9a52dee15aeac96389b
Silbeya <53601034+Silbeya@users.noreply.github.com>
2019-08-02 17:16:50 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-    if (message.content === 'gr.esta') {
+    if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
