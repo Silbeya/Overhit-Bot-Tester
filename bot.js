2d4426da4c105948d381244f8f5a526ff1da7424
Silbeya <53601034+Silbeya@users.noreply.github.com>
2019-08-04 19:48:37 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,13 +8,25 @@ client.on('ready', () => {
 });
 
 //COMMANDS
-client.on('message', msg => {
-    if (msg.content === '.test') {
-    	msg.reply('test good');
-    } 
+//client.on('message', msg => {
+//    if (msg.content === '.test') {
+//   	  msg.reply('test good');
+//    } 
      
-});
+//});
 
+let prefix = "!";
+client.on("message", msg => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+ 
+  if (msg.content.startsWith(prefix + "ping")) {
+    msg.channel.send("pong!");
+  } else
+  if (msg.content.startsWith(prefix + "foo")) {
+    msg.channel.send("bar!");
+  }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
