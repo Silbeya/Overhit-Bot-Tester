b4b61777018b0077294efa08de8018ce3c034add
Silbeya <53601034+Silbeya@users.noreply.github.com>
2019-08-04 20:06:12 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,21 +15,7 @@ client.on('ready', () => {
      
 //});
 
-client.on("message", message => {
-  if (message.author.bot) return;
-  // This is where we'll put our code.
-  if (message.content.indexOf(config.prefix) !== 0) return;
- 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
- 
-  if(command === 'ping') {
-    message.channel.send('Pong!');
-  } else
-  if (command === 'blah') {
-    message.channel.send('Meh.');
-  }
-});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
