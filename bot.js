bbe4a65ea7af87132f63c1ccf92b5a4749343a53
Silbeya <53601034+Silbeya@users.noreply.github.com>
2019-08-04 20:47:22 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,9 +21,9 @@ const embed = new Discord.RichEmbed()
   .setTitle("This is your title, it can hold 256 characters")
   .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
   /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   * Alternatively, use "0x00AE86", [0, 174, 134] or an integer number. 
    */
-  .setColor(0x00AE86)
+  .setColor(#00AE86)
   .setDescription("This is the main body of text, it can hold 2048 characters.")
   .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
   .setImage("http://i.imgur.com/yVpymuV.png")
